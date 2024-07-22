7b107aaea4f1b8657d7030bf0d43ad08be08cc26
Tassia Pereira <tassiapereira2020@gmail.com>
2024-07-22 00:16:51 -0300

    xadrezException e posicaoXadrez

--- src/Xadrez/PartidaXadrez.java
+++ src/Xadrez/PartidaXadrez.java
@@ -26,10 +26,14 @@ public class PartidaXadrez {
 		return mat;
 	}
 	
+	private void colocaNovaPeca( char coluna, int linha, PecaXadrez peca) {
+		tabuleiro.colocaPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
+	} 
+	
 	private void iniciaPartida() {
-		tabuleiro.colocaPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
-		tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0,4));
-		tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(7,4));
+		colocaNovaPeca( 'b', 6, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca( 'e', 8, new Rei(tabuleiro, Cor.PRETO));
+		colocaNovaPeca( 'e', 1, new Rei(tabuleiro, Cor.PRETO));
 	}
 	
 	
